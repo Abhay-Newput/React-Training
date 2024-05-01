be9ae8cabcb100be90c10ec41745b2123d28de5e
Abhay Agarwal <Abhay.agarwal@newput.com>
2024-05-01 10:21:37 +0530

    Dynamic Values in JSX

--- 01-starting-project/starting-project/src/App.js
+++ 01-starting-project/starting-project/src/App.js
@@ -1,12 +1,19 @@
 import logo from "./assets/react-core-concepts.png";
 
+const reactDescription = ["Fundamntal", "Concept", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescription[getRandomInt(2)];
   return (
     <header>
       <img src={logo} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
