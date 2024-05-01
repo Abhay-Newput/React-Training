9922cbd90dfcf0925ee3f18c7ea25634183d2dff
Abhay Agarwal <Abhay.agarwal@newput.com>
2024-05-01 10:49:35 +0530

    Props and Reusable Component

--- 01-starting-project/starting-project/src/App.js
+++ 01-starting-project/starting-project/src/App.js
@@ -1,4 +1,5 @@
 import logo from "./assets/react-core-concepts.png";
+import compImg from "./assets/components.png";
 
 const reactDescription = ["Fundamntal", "Concept", "Core"];
 
@@ -20,11 +21,34 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt="Props Title" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Component"
+              description="The core UI building block"
+              img={compImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
