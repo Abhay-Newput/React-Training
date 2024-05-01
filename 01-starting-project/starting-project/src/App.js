a7ab513ab81fffaadec73b93e0dbf5788804bca1
Abhay Agarwal <Abhay.agarwal@newput.com>
2024-05-01 16:39:58 +0530

    list content dynamically using map

--- 01-starting-project/starting-project/src/App.js
+++ 01-starting-project/starting-project/src/App.js
@@ -33,14 +33,17 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map((item) => (
+              <CoreConcept {...item} />
+            ))}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
